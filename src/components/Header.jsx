de776050bb8d1c26749fde43f6efeecfbf34ede7
Siar Wahidi <siar.wahidi@gmail.com>
2022-06-26 13:46:13 -0400

    added img to header and some styling to center

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import memeImg from "../assets/meme_img1.png";
 
 function Header() {
   return (
     <div className="header">
+      <img className="meme_img" src={memeImg} alt="meme head" />
       <h1>Random Meme Generator App</h1>
     </div>
   );
